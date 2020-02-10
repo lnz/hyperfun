3ec03e3800ddfb41d2982911f2fb918b95537a35
lorenzovonmatternhorn@gmx.ch <36513234+cem-okulmus@users.noreply.github.com>
2020-02-10 22:08:32 +0100

    added rudimentary colored HG output

--- hypergraph.py
+++ hypergraph.py
@@ -3,6 +3,15 @@ import re
 import pprint
 import itertools
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+
 
 class HyperGraph(object):
     def __init__(self):
@@ -142,4 +151,4 @@ class HyperGraph(object):
         return comps
 
     def __repr__(self):
-        return 'HG: {}'.format(pprint.pformat(self.edge_dict))
+        return bcolors.WARNING +  'HG: {}'.format(pprint.pformat(self.edge_dict))+ bcolors.ENDC
